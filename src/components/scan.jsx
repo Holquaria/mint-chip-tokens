cce46aaeaa7ce2786290cb55aed3153c8a82695e
Holquaria <jholker4@gmail.com>
2023-05-30 16:41:41 +0100

    initial commit to set up repo

---
+++ src/components/scan.jsx
@@ -0,0 +1,36 @@
+import { getPublicKeysFromScan, getSignatureFromScan } from 'pbt-chip-client/kong';
+import { useState } from 'react';
+
+const Scanner = () => {
+  const [keys, setKeys] = useState(null);
+  const [sig, setSig] = useState(null)
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          getPublicKeysFromScan().then((keys) => {
+            setKeys(keys);
+          });
+        }}
+      >
+        Click Me To Initiate Scan
+      </button>
+      <button
+        onClick={() => {
+          getSignatureFromScan({
+            chipPublicKey: keys.primaryPublicKeyRaw,
+            address: '<user_eth_address>',
+            hash: '<blockhash>',
+          }).then((sig) => {
+            setSig(sig);
+          });
+        }}
+      >
+        Click Me To Sign EOA+blockhash w/ Chip
+      </button>
+    </>
+  );
+}
+
+export default Scanner
