dfb7d08f7ac640ac50d492adf18bfd3c54ea7f27
Holquaria <jholker4@gmail.com>
2023-05-31 13:12:14 +0100

    update functions to get a block number and hash it

--- src/components/Scanner.jsx
+++ src/components/Scanner.jsx
@@ -1,9 +1,13 @@
 import { getPublicKeysFromScan, getSignatureFromScan } from 'pbt-chip-client/kong';
 import { useState } from 'react';
+import { ethers } from 'ethers';
+
 
 const Scanner = () => {
   const [keys, setKeys] = useState(null);
   const [sig, setSig] = useState(null)
+  const [blockNumber, setBlockNumber] = useState(0)
+  const [blockhash, setBlockhash] = useState('')
 
   console.log(keys?.primaryPublicKeyRaw)
 
@@ -13,7 +17,15 @@ const Scanner = () => {
         onClick={() => {
           getPublicKeysFromScan().then((keys) => {
             setKeys(keys);
-          });
+            return ethers.provider.getBlockNumber() - 1;
+          }).then((blockNumberUsedInSig) => {
+            console.log('setting block number', blockNumberUsedInSig)
+            setBlockNumber(blockNumberUsedInSig)
+            return ethers.provider.getBlock(blockNumberUsedInSig).then(block => block.hash);
+          }).then((blockhash) => {
+            console.log('setting blockhash', blockhash)
+            setBlockhash(blockhash)
+          })
         }}
       >
         Click Me To Initiate Scan
@@ -23,9 +35,10 @@ const Scanner = () => {
         onClick={() => {
           getSignatureFromScan({
             chipPublicKey: keys.primaryPublicKeyRaw,
-            address: '<user_eth_address>',
-            hash: '<blockhash>',
+            address: '0x5144c1D13a95Fb7A9714C53593B3957A75B79917',
+            hash: blockhash,
           }).then((sig) => {
+            console.log('setting sig', sig)
             setSig(sig);
           });
         }}
